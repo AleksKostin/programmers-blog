3dcf98841c6846b15709d5a25bde2fb083d68e98
Aleksey Kostin <alekkostin91@gmail.com>
2023-02-13 03:18:38 +0300

    Added babel

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -8,5 +8,6 @@ export function builDevServer(options: BuildOptions): DevServerConfiguration {
     port: options.port,
     open: true,
     historyApiFallback: true,
+    hot: true,
   };
 };
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,7 @@ import path from "path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import webpack from 'webpack';
+import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 import { BuildOptions } from "./types/config";
 
 export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
@@ -18,5 +19,7 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
     }),
+    new webpack.HotModuleReplacementPlugin(),
+    ...(isDev ? [new ReactRefreshWebpackPlugin()] : []),
   ];
 };

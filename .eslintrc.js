c8c1d4fda3c13a8e3c32583a5e77fa5a2f37be0b
Aleksey Kostin <alekkostin91@gmail.com>
2023-02-14 01:11:54 +0300

    Added NotFoundPage, PageLoader, Loader

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,7 +26,10 @@ module.exports = {
     'import/no-unresolved': 0,
     'import/extensions': 0,
     'react/function-component-definition': 0,
-    'react/jsx-filename-extension': [2, { extensions: ['.ts', '.tsx', '.js', '.jsx'] }],
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.ts', '.tsx', '.js', '.jsx'] },
+    ],
     'react/react-in-jsx-scope': 'off',
     'import/prefer-default-export': 'off',
     'no-unused-vars': 'warn',
@@ -36,6 +39,7 @@ module.exports = {
     'import/no-extraneous-dependencies': 'warn',
     'no-underscore-dangle': 'off',
     'i18next/no-literal-string': ['error', { markupOnly: true }],
+    'max-len': ['error', { ignoreComments: true }],
   },
   globals: {
     __IS_DEV__: true,
